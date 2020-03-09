cbda13e513e97079bc756aca7b38586ff6a887fa
joan <joan.herisson@univ-evry.fr>
2020-03-09 16:22:32 +0100

    merge

--- src/RetroPath2.py
+++ src/RetroPath2.py
@@ -8,6 +8,10 @@ Created on January 16 2020
 """
 
 
+import sys
+sys.path.insert(0, '/home/')
+import os
+import argparse
 import subprocess
 import logging
 import csv
@@ -15,9 +19,11 @@ import glob
 import resource
 import tempfile
 
+# logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 KPATH = '/usr/local/knime/knime'
-RP_WORK_PATH = '/home/workflow/RetroPath2.0.knwf'
+RP_WORK_PATH = '/home/workflows/9/RetroPath2.0.knwf'
 MAX_VIRTUAL_MEMORY = 20000*1024*1024 # 20 GB -- define what is the best
 
 ##
@@ -94,3 +100,48 @@ def run(sinkfile_bytes, sourcefile_bytes, max_steps, rules_bytes=b'None', topx=1
             logger.error('Cannot set the RAM usage limit')
             logger.error(e)
             return b'', b'ramerror', 'Command: '+str(knime_command)+'\n Error: '+str(e)+'\n tmpOutputFolder: '+str(glob.glob(tmpOutputFolder+'/*'))
+
+
+
+if __name__ == "__main__":
+    #### WARNING: as it stands one can only have a single source molecule
+    parser = argparse.ArgumentParser('Python wrapper for the KNIME workflow to run RetroPath2.0')
+    parser.add_argument('-sinkfile', type=str)
+    parser.add_argument('-sourcefile', type=str)
+    parser.add_argument('-max_steps', type=int)
+    parser.add_argument('-rulesfile', type=str)
+    parser.add_argument('-topx', type=int)
+    parser.add_argument('-dmin', type=int)
+    parser.add_argument('-dmax', type=int)
+    parser.add_argument('-mwmax_source', type=int)
+    parser.add_argument('-mwmax_cof', type=int)
+    parser.add_argument('-server_url', type=str)
+    parser.add_argument('-scope_csv', type=str)
+    parser.add_argument('-timeout', type=int)
+    parser.add_argument('-is_forward', type=str)
+    params = parser.parse_args()
+    if params.is_forward=='False' or params.is_forward=='false' or params.is_forward==False:
+        params.is_forward = False
+    elif params.is_forward=='True' or params.is_forward=='true' or params.is_forward==True:
+        params.is_forward = True
+    if (params.rulesfile==None) or (params.rulesfile==b'None') or (params.rulesfile=='None') or (params.rulesfile=='') or (params.rulesfile==b''):
+        params.rulesfile = os.getcwd()+'/empty_file.csv'
+        with open(params.rulesfile, 'wb') as ef:
+            ef.write(b'')
+    with open(params.sinkfile, 'rb') as sinkfile_bytes:
+        with open(params.sourcefile, 'rb') as sourcefile_bytes:
+            with open(params.rulesfile, 'rb') as rulesfile_bytes:
+                result = run(sinkfile_bytes.read(),
+                             sourcefile_bytes.read(),
+                             params.max_steps,
+                             rulesfile_bytes.read(),
+                             params.topx,
+                             params.dmin,
+                             params.dmax,
+                             params.mwmax_source,
+                             params.mwmax_cof,
+                             params.timeout,
+                             params.is_forward,
+                             logger)
+    with open(params.scope_csv, 'wb') as s_c:
+        s_c.write(result[0])
