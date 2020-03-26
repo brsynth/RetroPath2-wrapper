f6cc65f0897da38b11ec29f0bf7f3f03edc3cf8d
joan <joan.herisson@univ-evry.fr>
2020-03-26 18:34:46 +0100

    rm print command

--- src/RetroPath2.py
+++ src/RetroPath2.py
@@ -62,8 +62,6 @@ def run(sinkfile, sourcefile, max_steps, rulesfile, outdir, topx=100, dmin=0, dm
             + ' -workflow.variable=output.solutionfile,"results.csv",String' \
             + ' -workflow.variable=output.sourceinsinkfile,"source-in-sink.csv",String'
 
-        print(knime_command)
-
         commandObj = subprocess.Popen(knime_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, preexec_fn=limit_virtual_memory)
 
         try:
