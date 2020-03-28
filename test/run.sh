4f0dc4159c6a19c34bb4689218c95884993753bf
joan <joan.herisson@univ-evry.fr>
2020-03-28 19:56:47 +0100

    rulesfile

--- test/run.sh
+++ test/run.sh
@@ -10,7 +10,7 @@ python3 ../src/RetroPath2.py \
   -sinkfile in/Galaxy177-Sink_Compounds.csv \
   -sourcefile in/Galaxy160-Source.csv \
   -max_steps 3 \
-  -rulesfile in/_exclude_rules.csv \
+  -rulesfile in/empty_file.csv \
   -topx 100 \
   -dmin 0 \
   -dmax 1000 \
