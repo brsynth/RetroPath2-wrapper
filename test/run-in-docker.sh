9a792a92dc541b2f6ed9d8c5067ea5a68b9af79f
joan <joan.herisson@univ-evry.fr>
2020-06-03 16:05:01 +0200

    rm out folder arg

--- test/run-in-docker.sh
+++ test/run-in-docker.sh
@@ -2,24 +2,5 @@
 
 cd ../docker
 docker-compose run --rm -v $PWD/../test:/home/test -w /home/test --entrypoint="" retropath2 \
-  sh -c "./run.sh out/test-in-docker"
+  sh -c "./run.sh"
 cd -
-
-
-# docker run \
-#   -v $PWD/..:/home \
-#   -w /home/test \
-#   --rm brsynth/retropath2 \
-# /bin/sh test-standalone.sh
-
-#cp results/test_out_scope.csv .
-
-
-# docker run \
-#   -v ${PWD}/run.sh:/home/run.sh \
-#   -v ${PWD}/tool_RetroPath2.py:/home/tool_RetroPath2.py \
-#   -v ${PWD}/test_input_sink.dat:/home/test_input_sink.dat \
-#   -v ${PWD}/test_input_source.dat:/home/test_input_source.dat \
-#   -v ${PWD}/results/:/home/results/ \
-#   --rm brsynth/retropath2 \
-# /bin/sh /home/run.sh
--- test/run.sh
+++ test/run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-outdir=$1
-
-if [[ "$outdir" == "" ]]; then
-  outdir="out/test"
-fi
-
 python3 ../src/RetroPath2.py \
   -sinkfile in/Galaxy177-Sink_Compounds.csv \
   -sourcefile in/Galaxy160-Source.csv \
@@ -17,10 +11,5 @@ python3 ../src/RetroPath2.py \
   -mwmax_source 1000 \
   -mwmax_cof 1000 \
   -timeout 30 \
-  -outdir $outdir \
+  -outdir out \
   -is_forward False
-
-#mv test_out_scope.csv out/
-
-# -sinkfile test_input_sink.dat \
-# -sourcefile test_input_source.dat \
