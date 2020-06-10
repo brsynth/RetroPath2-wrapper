346108cf762e93a81f77302ec0cd806f0f761adc
joan <joan.herisson@univ-evry.fr>
2020-06-10 14:19:34 +0200

    args

--- test/run.sh
+++ test/run.sh
@@ -4,7 +4,7 @@ python3 ../src/RetroPath2.py \
   -sinkfile in/Galaxy177-Sink_Compounds.csv \
   -sourcefile in/Galaxy160-Source.csv \
   -max_steps 3 \
-  -rulesfile in/empty_file.csv \
+  -rulesfile in/_exclude_rules.csv \
   -topx 100 \
   -dmin 0 \
   -dmax 1000 \
@@ -13,3 +13,7 @@ python3 ../src/RetroPath2.py \
   -timeout 30 \
   -outdir out \
   -is_forward False
+
+  # -rulesfile in/empty_file.csv \
+  # -rulesfile in/rules-d2.csv \
+  # -rulesfile in/_exclude_rules.csv \
