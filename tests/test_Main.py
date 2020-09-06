177c7430a6f9c23935aabe7f46a59a038efcd8b2
joan <joan.herisson@univ-evry.fr>
2020-09-06 23:25:29 +0200

    add tests

--- tests/test_Main.py
+++ tests/test_Main.py
@@ -6,7 +6,6 @@ Created on Jul 15 2020
 
 from unittest  import TestCase
 from os import path as os_path
-from brs_utils import extract_gz
 
 from retropath2_wrapper import run, build_args_parser
 # from tempfile import TemporaryDirectory
@@ -29,7 +28,9 @@ class Test_Main(TestCase):
         -is_forward False'.split()
     args  = build_args_parser().parse_args(cmd)
 
-    def test_run(self):
+    def test_from_scratch(self):
+
+        from brs_utils import extract_gz
 
         if not os_path.exists(self.args.rulesfile):
             extract_gz('data/rules.tar.gz', 'data')
@@ -46,4 +47,73 @@ class Test_Main(TestCase):
                      self.args.mwmax_cof,
                      self.args.timeout,
                      self.args.is_forward)
+        self.assertEqual(True, True)
+
+
+    def test_with_knime_installed_with_right_path(self):
+
+        from subprocess import call, STDOUT, TimeoutExpired# nosec
+        from brs_utils import download_and_extract_gz, extract_gz
+
+        if not os_path.exists(self.args.rulesfile):
+            extract_gz('data/rules.tar.gz', 'data')
+
+        KVER         = '3.6.2'
+        KURL         = 'http://download.knime.org/analytics-platform/linux/knime_'+KVER+'.linux.gtk.x86_64.tar.gz'
+        KINSTALL     = '/tmp'
+        KPATH        = KINSTALL+'/knime_'+KVER
+        KEXEC        = KPATH+'/knime'
+
+        download_and_extract_gz(KURL, '/tmp')
+        knime_add_pkgs = KEXEC \
+            + ' -application org.eclipse.equinox.p2.director' \
+            + ' -nosplash -consolelog' \
+            + ' -r http://update.knime.org/community-contributions/trunk,' \
+                + 'http://update.knime.com/analytics-platform/3.6,' \
+                + 'http://update.knime.com/community-contributions/trusted/3.6' \
+            + ' -i org.knime.features.chem.types.feature.group,' \
+                + 'org.knime.features.datageneration.feature.group,' \
+                + 'jp.co.infocom.cheminfo.marvin.feature.feature.group,' \
+                + 'org.knime.features.python.feature.group,' \
+                + 'org.rdkit.knime.feature.feature.group' \
+            + ' -bundlepool '+KPATH+' -d '+KPATH
+        call(knime_add_pkgs.split(), stderr=STDOUT, shell=False)# nosec
+
+        result = run(self.args.sinkfile,
+                     self.args.sourcefile,
+                     self.args.max_steps,
+                     self.args.rulesfile,
+                     self.args.outdir,
+                     self.args.topx,
+                     self.args.dmin,
+                     self.args.dmax,
+                     self.args.mwmax_source,
+                     self.args.mwmax_cof,
+                     self.args.timeout,
+                     self.args.is_forward,
+                     KEXEC)
+        self.assertEqual(True, True)
+
+
+    def test_wrong_knime_path(self):
+
+        from os import path as os_path
+        from brs_utils import extract_gz
+
+        if not os_path.exists(self.args.rulesfile):
+            extract_gz('data/rules.tar.gz', 'data')
+
+        result = run(self.args.sinkfile,
+                     self.args.sourcefile,
+                     self.args.max_steps,
+                     self.args.rulesfile,
+                     self.args.outdir,
+                     self.args.topx,
+                     self.args.dmin,
+                     self.args.dmax,
+                     self.args.mwmax_source,
+                     self.args.mwmax_cof,
+                     self.args.timeout,
+                     self.args.is_forward,
+                     '/tmp/knime/knime')
         self.assertEqual(True, True)
