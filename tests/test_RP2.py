0a7b40cf8808cfc51682f44d9367d18c33ebcc7b
joan <joan.herisson@univ-evry.fr>
2020-11-26 15:14:36 +0100

    test(input): change input parameters

--- tests/test_RP2.py
+++ tests/test_RP2.py
@@ -8,7 +8,7 @@ from unittest  import TestCase
 from tempfile  import TemporaryDirectory, gettempdir
 from os        import path      as os_path
 from filecmp   import cmp
-from brs_utils          import extract_tar_gz
+from brs_utils          import extract_gz
 from retropath2_wrapper import retropath2
 
 
@@ -16,16 +16,12 @@ from retropath2_wrapper import retropath2
 class Test_RP2(TestCase):
 
     data_path  = 'data'
-    sinkfile   = os_path.join(data_path,    'sink.csv')
-    sourcefile = os_path.join(data_path,    'source.csv')
-    rulesfile  = os_path.join(gettempdir(), 'rules.csv')
-    ref_file   = os_path.join(data_path,    'results.csv')
-    outdir = TemporaryDirectory().name
-    outdir = '/tmp/joan'
-
-    def setUp(self):
-        if not os_path.exists(self.rulesfile):
-            extract_tar_gz(os_path.join(self.data_path, 'rules.tar.gz'), gettempdir())
+    sinkfile   = os_path.join(data_path, 'sink.csv')
+    sourcefile = os_path.join(data_path, 'source.csv')
+    rulesfile  = extract_gz(os_path.join(data_path, 'rules-d16.csv.gz'),
+                            gettempdir())
+    ref_file   = os_path.join(data_path, 'scope.csv')
+    outdir     = TemporaryDirectory().name
 
 
     def test_light(self):
@@ -35,7 +31,5 @@ class Test_RP2(TestCase):
                              self.rulesfile,
                              self.outdir,
                              dmin=16)
-
-        print(outFile)
 
         self.assertTrue(cmp(outFile, self.ref_file))
