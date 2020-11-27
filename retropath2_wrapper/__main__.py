1a80baac76dfc9c699a70eba6296ad52b8a1329f
joan <joan.herisson@univ-evry.fr>
2020-11-27 17:27:22 +0100

    chore(main): mv rules gzip format handling into 'retropath2()' function

--- retropath2_wrapper/__main__.py
+++ retropath2_wrapper/__main__.py
@@ -4,17 +4,12 @@
 from os       import getcwd     as os_getcwd
 from tempfile import gettempdir
 
-from brs_utils          import extract_gz
 from retropath2_wrapper import retropath2, build_args_parser
 
 
 def _cli():
     parser = build_args_parser()
     args  = parser.parse_args()
-
-    # If rulesfile is a pure gzip archive without tar
-    if args.rulesfile.endswith('.gz') and '.tar.' not in args.rulesfile:
-        args.rulesfile = extract_gz(args.rulesfile, gettempdir())
 
     outFile = retropath2(args.sinkfile,
                          args.sourcefile,
