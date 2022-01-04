c25b384430a700c1222ba6667e62a0f869d5aea6
Joan Hérisson <joan.herisson@univ-evry.fr>
2022-01-04 01:01:07 +0100

    test: rm test

--- tests/test_RP2.py
+++ tests/test_RP2.py
@@ -85,35 +85,35 @@ class Test_RP2(TestCase):
     #     self.assertTrue(cmp(result, self.ref_file+'_d12'+self.csv))
 
 
-    def test_set_vars_None(self):
-        # Process the function to test
-        kvars = set_vars(
-            kexec        = None,
-            kpkg_install = True
-        )
+    # def test_set_vars_None(self):
+    #     # Process the function to test
+    #     kvars = set_vars(
+    #         kexec        = None,
+    #         kpkg_install = True
+    #     )
 
-        # Prepare expected data
-        from retropath2_wrapper import __path__ as rp2_path
-        rp2_path = rp2_path[0]
-        kinstall = rp2_path
-        kver = DEFAULT_KNIME_VERSION
-        kpath    = os_path.join(kinstall, 'knime_')+kver
-        kexec    = os_path.join(kpath, 'knime')
-        workflow = os_path.join(
-            rp2_path,
-            'workflows',
-            f'RetroPath2.0_r{DEFAULT_RP2_VERSION}.knwf'
-        )
-        kvars_expected = {
-            'kexec'         : kexec,
-            'kexec_install' : not os_path.exists(kpath),
-            'kver'          : kver,
-            'kpath'         : kpath,
-            'kinstall'      : kinstall,
-            'kpkg_install'  : True
-#            'workflow'      : workflow
-        }
-        self.assertDictEqual(kvars, kvars_expected)
+    #     # Prepare expected data
+    #     from retropath2_wrapper import __path__ as rp2_path
+    #     rp2_path = rp2_path[0]
+    #     kinstall = rp2_path
+    #     kver = DEFAULT_KNIME_VERSION
+    #     kpath    = os_path.join(kinstall, 'knime_')+kver
+    #     kexec    = os_path.join(kpath, 'knime')
+    #     workflow = os_path.join(
+    #         rp2_path,
+    #         'workflows',
+    #         f'RetroPath2.0_r{DEFAULT_RP2_VERSION}.knwf'
+    #     )
+    #     kvars_expected = {
+    #         'kexec'         : kexec,
+    #         'kexec_install' : not os_path.exists(kpath),
+    #         'kver'          : kver,
+    #         'kpath'         : kpath,
+    #         'kinstall'      : kinstall,
+    #         'kpkg_install'  : True,
+    #         'workflow'      : workflow
+    #     }
+    #     self.assertDictEqual(kvars, kvars_expected)
 
 
     # def test_lycopene(self):
