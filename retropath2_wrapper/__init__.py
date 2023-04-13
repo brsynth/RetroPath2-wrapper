79bd726aac6766f87b6f024d44de342990e30673
Joan Hérisson <joan.herisson@univ-evry.fr>
2023-04-14 00:30:22 +0200

    chore(build): rm windows

--- retropath2_wrapper/__init__.py
+++ retropath2_wrapper/__init__.py
@@ -9,4 +9,5 @@ from retropath2_wrapper.Args       import build_args_parser
 from retropath2_wrapper._version   import __version__
 from retropath2_wrapper.__main__   import parse_and_check_args
 
+
 __all__ = ["retropath2", "build_args_parser"]
