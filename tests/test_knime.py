491327805a7d6f59dcc396444449a3379418100f
guillaume-gricourt <guipagui@gmail.com>
2025-10-06 10:07:58 +0200

    tests(knime): update to fit the features

--- tests/test_knime.py
+++ tests/test_knime.py
@@ -5,10 +5,7 @@ import subprocess
 import tempfile
 
 import pytest
-from retropath2_wrapper.Args import (
-    KNIME_ZENODO,
-    RETCODES
-)
+from retropath2_wrapper.Args import RETCODES
 from retropath2_wrapper.knime import Knime
 
 
@@ -43,29 +40,10 @@ class TestKnime:
         assert "\\" not in spath
 
     @pytest.mark.skipif(FUNCTIONAL, reason="Functional test")
-    def test_install_knime_from_knime(self):
-        tempdir = tempfile.mkdtemp()
-
-        knime = Knime(workflow="", kinstall=tempdir)
-        knime.install_exec()
-        kexec = TestKnime.filter_exec(path=tempdir)
-        assert kexec is not None
-        # Failed could be araise due to missing dependecy
-        try:
-            ret = knime.install_pkgs()
-            assert ret == RETCODES['OK']
-        except Exception:
-            pass
-        shutil.rmtree(tempdir, ignore_errors=True)
-
-    @pytest.mark.skipif(FUNCTIONAL, reason="Functional test")
     def test_install_knime_from_zenodo(self):
         tempdir = tempfile.mkdtemp()
-
-        knime = Knime(workflow="", kinstall=tempdir, kver=list(KNIME_ZENODO.keys())[0])
-        knime.install_exec()
+        knime = Knime(workflow="", kinstall=tempdir)
+        knime.install(kver=list(Knime.ZENODO.keys())[0])
         kexec = TestKnime.filter_exec(path=tempdir)
         assert kexec is not None
-        ret = knime.install_pkgs()
-        assert ret == RETCODES['OK']
         shutil.rmtree(tempdir, ignore_errors=True)
